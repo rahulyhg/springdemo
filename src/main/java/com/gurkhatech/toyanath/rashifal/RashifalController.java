1ed78a091a619b06c4036e815af3518ed267f240
Laxman Bhattarai <erlaxmanbhattarai@gmail.com>
2017-06-08 21:43:15 +0545

    did some rest API

--- src/main/java/com/gurkhatech/toyanath/rashifal/RashifalController.java
+++ src/main/java/com/gurkhatech/toyanath/rashifal/RashifalController.java
@@ -1,7 +1,12 @@
 package com.gurkhatech.toyanath.rashifal;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,16 +15,42 @@ public class RashifalController {
 	@Autowired
 	private RashifalService service;
 
-	@RequestMapping("rashifal/daily")
-	public DailyRashifalEntity getDailyRashifal() {
-		DailyRashifalEntity drm = service.getDaily(3+"");
-		if (drm != null) {
-			System.out.println("!Not NULL");
-			return drm;
-		}
-		System.out.println("NULL");
-		return null;
+	@RequestMapping("rashifal/daily/{id}")
+	public DailyRashifalEntity getDailyRashifal(@PathVariable String id) {
+		return service.getDaily(2017, 2, 3, id);
 	}
 
+	@RequestMapping("rashifal/daily")
+	public List<DailyRashifalEntity> getDailyRashifalAll() {
+		return service.getDailyAll(2017, 2, 3);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "rashifal/daily")
+	public void addRashifalAll(@RequestBody List<DailyRashifalEntity> entityList) {
+		service.addDailyAll(entityList);
+
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "rashifal/daily/")
+	public void updateRashifalAll(@RequestBody List<DailyRashifalEntity> entityList) {
+		service.updateAll(entityList);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "rashifal/daily/delete_all")
+	public void deleteDailyAll(@RequestBody List<DailyRashifalEntity> entityList) {
+		service.deleteDailyAll(entityList);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "rashifal/daily/delete_all_by_day")
+	public void deleteDailyAllByDay(@PathVariable("year") int year, @PathVariable("month") int month,
+			@PathVariable("day") int day) {
+		service.deleteDailyAllByDay(year, month, day);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "rashifal/daily/delete_by_day")
+	public void deleteDailyByDay( @PathVariable("year") int year,
+			@PathVariable("month") int month, @PathVariable("day") int day,@PathVariable("id") String id) {
+		service.deleteDailyByDay(year, month, day, id);
+	}
 
 }
--- src/main/java/com/gurkhatech/toyanath/rashifal/RashifalService.java
+++ src/main/java/com/gurkhatech/toyanath/rashifal/RashifalService.java
@@ -1,34 +1,69 @@
 package com.gurkhatech.toyanath.rashifal;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class RashifalService {
-	
-	@Autowired	DailyRashifalRepository dailyRepo;
-	@Autowired	WeeklyRashifalRepository weeklyRepo;
-	@Autowired	MonthlyRashifalRepository monthlyRepo;
-	@Autowired	YearlyRashifalRepository yearlyRepo;
-	
-	public DailyRashifalEntity getDaily(int year,int month,int day,int rashi){
-		return dailyRepo.findOne(null);
+
+	@Autowired
+	DailyRashifalRepository dailyRepo;
+	@Autowired
+	WeeklyRashifalRepository weeklyRepo;
+	@Autowired
+	MonthlyRashifalRepository monthlyRepo;
+	@Autowired
+	YearlyRashifalRepository yearlyRepo;
+
+	public DailyRashifalEntity getDaily(int year, int month, int day, String id) {
+		return dailyRepo.findOne("");
 	}
-	
-	public List<DailyRashifalEntity> getAllDaily(int year,int month,int day){
+
+	public List<DailyRashifalEntity> getDailyAll(int year, int month, int day) {
 		List<DailyRashifalEntity> rashiList = new ArrayList<DailyRashifalEntity>();
+		dailyRepo.findAll().forEach(rashiList::add);
 		return rashiList;
 	}
-	
-	public DailyRashifalEntity getDaily(String id ){
-		dailyRepo.save(new DailyRashifalEntity("1", "aaaja mashu vat khana paine yog chha", 2074, 1, 2));
-		return dailyRepo.findOne(id+"");
+
+	public void addDaily(DailyRashifalEntity entity) {
+		dailyRepo.save(entity);
+	}
+	public void addDailyAll(List<DailyRashifalEntity> entityList) {
+		for(int i=0;i<entityList.size();i++){
+			dailyRepo.save(entityList.get(i));
+		}
+	}
+
+	public void updateDaily(DailyRashifalEntity entity) {
+		//dailyRepo.
 	}
 	
+	public void updateAll(List<DailyRashifalEntity> rashifalList){
+		for(int i=0;i<rashifalList.size();i++){
+			updateDaily(rashifalList.get(i));
+		}
+	}
+	public void deleteDaily(DailyRashifalEntity entity ) {
+		dailyRepo.delete(entity);
+	}
 	
+	public void deleteDailyAll(List<DailyRashifalEntity> entityList ) {
+		for(int i=0;i<entityList.size();i++){
+			dailyRepo.delete(entityList.get(i));
+		}
+	}
+	public void deleteDailyAllByDay(int year,int month, int day){
+		// TODO use query for a single day dailyRepo.deleteAll();
+	}
+	
+	public void deleteDailyByDay(int year,int month, int day,String id){
+		// TODO use query for a single day dailyRepo.deleteAll();
+	}
+	public void deleteDailyAll(){
+		dailyRepo.deleteAll();
+	}
+
 }
